cfb6c2f6ab459c34785e74dd0892ff3497829d49
Martin <martin.pernus1@gmail.com>
2022-09-17 21:24:51 +0200

    Refactoring of attribute classifier, the code is now much more readable. The script for downloading script now downloads a different state dict with the same weights but different key structure to accomodate the model code redefinition.

--- download.sh
+++ download.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 echo "Downloading attribute classifier checkpoint"
-wget -O models/attribute_classifier/BranchedTiny.ckpt https://www.dropbox.com/s/nbm6oi70x158sn3/BranchedTiny.ckpt?dl=1
+wget -O models/attribute_classifier/state.pt https://www.dropbox.com/s/4j1x9zhri8cndny/state.pt?dl=1
+
 
 echo "Downloading face parser checkpoint"
-wget -O models/face_parser/FaceParser.ckpt https://www.dropbox.com/s/oe45ovn0we7ynh8/FaceParser.ckpt?dl=1
+wget -O models/face_parser/state.ckpt https://www.dropbox.com/s/oe45ovn0we7ynh8/FaceParser.ckpt?dl=1
 
 echo "Downloading e4e encoder"
 wget -O models/e4e/state.pt https://www.dropbox.com/s/t8tm5tbk27wa7er/state.pt?dl=1
